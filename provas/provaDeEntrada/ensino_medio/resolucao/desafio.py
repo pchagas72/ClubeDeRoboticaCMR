9cdae185a70c1f3086a316d921a216cdd3599492
PedroChagas <pchagas.cloud@gmail.com>
2023-02-25 22:15:05 -0300

    Editei o código do desafio para ficar mais 'beginner friendly'

--- provas/provaDeEntrada/ensino_medio/resolucao/desafio.py
+++ provas/provaDeEntrada/ensino_medio/resolucao/desafio.py
@@ -1,10 +1,10 @@
-"""This is the python code for the 16th challange in projecteuler.com"""
+"""
+Essa é uma maneira bem "pythonica" de resolver este problema, se você não entender, está tudo bem!
+Pode falar comigo (chagas) se quiser que eu explique.
+"""
 
 
 def solve(base: int, power: int) -> int:
-    """
-    Very pythonic way of doing this, I like it (:
-    """
     return sum(int(i) for i in list(str(base**power)))
 
 
--- provas/provaDeEntrada/ensino_medio/resolucao/multiplos_de_3_ou_5.py
+++ provas/provaDeEntrada/ensino_medio/resolucao/multiplos_de_3_ou_5.py
@@ -1,9 +1,9 @@
 def solve(limit: int) -> int:
-    sum = 0
+    soma = 0
     for i in range(limit):
         if i % 3 == 0 or i % 5 == 0:
-            sum += i
-    return sum
+            soma += i
+    return soma
 
 if __name__ == '__main__':
-    solve(1000)
+    print(solve(1000))
